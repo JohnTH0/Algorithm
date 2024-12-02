e64983ebd6047af230789a577a1637b338a0b74b
TH <ok821000@gmail.com>
2024-12-02 23:54:22 +0900

    [Algorithm] 문제 풀이 - 리트코드 125번

---
+++ leetcode/main_125.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class main_125 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        LinkedList<String> stack = new LinkedList<>();
+        if(s.length() == 2){
+
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLetter(c)) {
+                stack.add(Character.toString(c).toLowerCase());
+            } else if (Character.isDigit(c)) {
+                stack.add(Character.toString(c));
+            }
+        }
+
+        while (stack.size() > 1) {
+            if (!Objects.equals(stack.getFirst(), stack.getLast())) {
+                System.out.println("False");
+                System.exit(0);
+            }
+            stack.removeFirst();
+            stack.removeLast();
+        }
+        System.out.println("True");
+    }
+}
+
+/* 제출코드
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Objects;
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        LinkedList<String> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLetter(c)) {
+                stack.add(Character.toString(c).toLowerCase());
+            } else if (Character.isDigit(c)) {
+                stack.add(Character.toString(c));
+            }
+        }
+        while (stack.size() > 1) {
+            if (!Objects.equals(stack.getFirst(), stack.getLast())) {
+                return false;
+            }
+            stack.removeFirst();
+            stack.removeLast();
+        }
+        return true;
+
+    }
+}
+ */
