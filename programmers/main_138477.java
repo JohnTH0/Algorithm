90a6d02e2e56e216c3f9e4e50416092e7584d996
TH <ok821000@gmail.com>
2024-12-05 23:57:09 +0900

    [Algorithm] 문제 풀이 - 프로그래머스 138477번

---
+++ programmers/main_138477.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class main_138477 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] sArray = br.readLine().split(" ");
+        int k = Integer.parseInt(br.readLine());
+        int[] score = new int[sArray.length];
+        int[] solArray = new int[sArray.length];
+        List<Integer> rank = new ArrayList<>();
+
+        for (int i = 0; i < sArray.length; i++) {
+            score[i] = Integer.parseInt(sArray[i]);
+        }
+
+        for (int i = 0; i < score.length; i++) {
+            rank.add(score[i]);
+            if (rank.size() > k) {
+                rank.remove(Collections.min(rank));
+            }
+            solArray[i] = Collections.min(rank);
+        }
+    }
+}
+/* 제출 코드
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] solArray = new int[score.length];
+        List<Integer> rank = new ArrayList<>();
+
+        for (int i = 0; i < score.length; i++) {
+            rank.add(score[i]);
+            if (rank.size() > k) {
+                rank.remove(Collections.min(rank));
+            }
+            solArray[i] = Collections.min(rank);
+        }
+        return solArray;
+    }
+}
+ */
