e121b6e7b55df3aa9dae56b4628446b96d282d37
TH <ok821000@gmail.com>
2025-01-14 13:50:33 +0900

    [Algorithm] 문제풀이 - 프로그래머스 12941번

---
+++ programmers/Main_12941.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_12941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arrA = createIntArray(br.readLine());
+        int[] arrB = createIntArray(br.readLine());
+        int arrayLength = arrA.length;
+
+        long result = 0;
+
+        Arrays.sort(arrA);
+        Arrays.sort(arrB);
+
+        for (int i = 0; i < arrA.length; i++) {
+            result += (long) arrA[i] * arrB[arrayLength - 1 - i];
+        }
+        System.out.println("result = " + result);
+
+
+
+    }
+    public static int[] createIntArray(String input) {
+        String[] strArr = input.split(" ");
+        int[] arr = new int[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            arr[i] = Integer.parseInt(strArr[i]);
+        }
+        return arr;
+    }
+}
