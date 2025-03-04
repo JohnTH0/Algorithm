bebf6693120dd9c0168b1995ac059a208c1796cc
TH <ok821000@gmail.com>
2025-03-04 18:06:23 +0900

    [Algorithm] 문제 풀이 - 프로그래머스 12915, 132267번

---
+++ programmers/Main_12915.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main_12915 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = br.readLine().split(" ");
+        int n = Integer.parseInt(br.readLine());
+        Arrays.sort(strings, Comparator.comparingInt((String s) -> s.charAt(n)).thenComparing(Comparator.naturalOrder()));
+        System.out.println(Arrays.toString(strings));
+
+    }
+}
+
+/*
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings);
+        Arrays.sort(strings, new Comparator<String>(){
+            @Override
+            public int compare(String s1, String s2){
+                if(s1.charAt(n) > s2.charAt(n)) return 1;
+                else if(s1.charAt(n) < s2.charAt(n)) return -1;
+                else return 0;
+            }
+        });
+        return strings;
+    }
+}
+*/
